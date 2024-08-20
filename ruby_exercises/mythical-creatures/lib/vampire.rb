5a82cb9a711f5a819e47ca9c9b62232bb2c98c76
Seth Verrill <verrill.seth@gmail.com>
2024-08-20 16:55:11 -0600

    Added code to pass rspec test

--- ruby_exercises/mythical-creatures/lib/vampire.rb
+++ ruby_exercises/mythical-creatures/lib/vampire.rb
@@ -1,7 +1,15 @@
 class Vampire
-  attr_reader :name
+  attr_reader :name,
+              :pet
+  attr_accessor :thirsty
 
-  def initialize (name)
+  def initialize (name, pet = 'bat')
   @name = name
+  @pet = pet
+  @thirsty = true
+  end
+
+  def drink
+    @thirsty = false
   end
 end
