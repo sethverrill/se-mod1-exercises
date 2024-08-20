94b7c57889fcc98e96bc4d17c2c8d48125128b25
Seth Verrill <verrill.seth@gmail.com>
2024-08-20 14:35:19 -0600

    Add code and test elements to make assignment pass

---
+++ lessons/intro_to_testing/lib/student.rb
@@ -0,0 +1,13 @@
+class Student
+  attr_reader :name,
+              :cookies
+
+  def initialize(name)
+    @name = name
+    @cookies = []
+  end
+
+  def add_cookie(cookie)
+   @cookies << cookie
+  end
+end      
--- lessons/intro_to_testing/spec/student_spec.rb
+++ lessons/intro_to_testing/spec/student_spec.rb
@@ -1,13 +1,42 @@
 # Add the 2 requires you will need here
+require 'rspec'
+require './lib/student'
 
 
-describe Student do
-  describe '#initialize' do
-    it 'is an instance of student' do
+
+describe Student do #the class should be the exact same name as the class we're testing
+  describe '#initialize' do #the name of the method we're describing
+    it 'is an instance of student' do #describing what the method should do/return
+      student = Student.new('Penelope')
       # write the code to initialize a new student object
+      expect(student).to be_a Student #assertion
+      
     end
     # test it has a name
+    it 'is given a name' do
+      student = Student.new('Penelope')
+
+      expect(student.name).to eq("Penelope")
+    end
     # test it has cookies
+    it 'has an empty array of cookies' do
+      student = Student.new('Penelope')
+
+      expect(student.cookies).to eq([])
+    end
     # test it can add cookies
   end
+  describe "#add_cookie" do
+    it "adds a cookie to our @cookies list" do
+      student = Student.new('Penelope')
+      student.add_cookie('Chocolate Chunk')
+      student.add_cookie('Snickerdoodle')
+
+      expect(student.cookies).to eq(['Chocolate Chunk', 'Snickerdoodle'])
+    
+    end
+  end
+
+
+  
 end
