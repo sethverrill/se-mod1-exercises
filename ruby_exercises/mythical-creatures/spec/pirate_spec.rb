81ad5893449ff393a10e7bde66af071ee5002fc2
Seth Verrill <verrill.seth@gmail.com>
2024-09-14 12:19:49 -0600

    test: Add tests for aquiring wealth

--- ruby_exercises/mythical-creatures/spec/pirate_spec.rb
+++ ruby_exercises/mythical-creatures/spec/pirate_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require './lib/pirate'
 
-RSpec.describe Pirate do
+RSpec.describe Pirate do  
   it 'has a name' do
     pirate = Pirate.new('Jane')
     expect(pirate.name).to eq('Jane')
@@ -38,14 +38,15 @@ RSpec.describe Pirate do
   end
 
   it 'has a booty' do
-    # create a pirate
-    # check that the pirate starts with 0 booty
+   pirate = Pirate.new('Larry')
+   expect(pirate.booty).to eq(0)
   end
 
   it 'gets 100 booty for robbing a ship' do
-    # create a pirate
-    # rob some ships
-    # check that the pirate got 100 booty for each ship it robbed
+    pirate = Pirate.new('Larry')
+    pirate.robs_ship
+    expect(pirate.booty).to eq(100)
+    2.times { pirate.robs_ship }
+    expect(pirate.booty).to eq(300)
   end
-
 end
