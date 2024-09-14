1433fb84bdb5b46d58841f5c3ebe8c70c932d941
Seth Verrill <verrill.seth@gmail.com>
2024-09-14 11:59:07 -0600

    feat: Add methods to control patron number to complete assignment requirements

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,29 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    @patrons.map(&:upcase)    
+  end
+
+  def over_capacity?
+    @patrons.length > @capacity
+  end
+
+  def kick_out
+    while over_capacity?
+      @patrons.pop
+    end
+  end
 end
